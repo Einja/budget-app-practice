db90cc85656a4bc26787649ba4b86f590bb47a57
Einja <ethanfan1033@gmail.com>
2024-10-18 12:33:23 -0400

    user model defined

---
+++ backend/models/User.js
@@ -0,0 +1,21 @@
+// Script sets up a basic user structure in MongoDB for when we create users
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+module.exports = mongoose.model('User', userSchema);
